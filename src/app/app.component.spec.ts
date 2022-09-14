87a240ace830f66406da6956d3ad63f7ec4f181a
Tomasz Grochocki <tgrochocki@future-processing.com>
2022-09-14 09:54:19 +0200

    Add Angular Material NgMock and test for MatTooltipDirective

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+import { AppModule } from './app.module';
+import { MatTooltip } from '@angular/material/tooltip';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  beforeEach(() => {
+    return MockBuilder(AppComponent, AppModule);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should contain proper p text', () => {
+    const fixture = MockRender(AppComponent);
+
+    expect(fixture.nativeElement.innerHTML).toContain('NgMock MatTooltip');
   });
 
-  it(`should have as title 'angular-starter'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-starter');
+  it('should check MatTooltip Directive message', () => {
+    const fixture = MockRender(AppComponent);
+
+    const matTooltip = ngMocks.findInstance(MatTooltip);
+
+    expect(matTooltip.message).toContain('Info about the action');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-starter app is running!');
+  it('should check MatTooltip Directive message', () => {
+    const fixture = MockRender(AppComponent);
+
+    const matTooltip = ngMocks.findInstance(MatTooltip);
+
+    expect(matTooltip.disabled).toBeFalse();
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MatButtonModule,
+    MatTooltipModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
